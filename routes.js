5999e00c6e8277abaeb8b851612dbc9a76922219
Carlos H <carlos.henrique@segsat.com>
2020-08-12 18:22:34 -0300

    primeiro fix

--- routes.js
+++ routes.js
@@ -6,20 +6,4 @@ const router = express.Router();
 router.get('/',(req,res)=>{
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
-
-router.get('/entrar',(req,res)=>{
-  res.sendFile(path.join(__dirname+'/public/entrar.html'));
-});
-
-router.get('/exercicio',(req,res)=>{
-  res.sendFile(path.join(__dirname+'/public/exercicio.html'));
-});
-
-router.get('/rotas',(req,res)=>{
-  res.sendFile(path.join(__dirname+'/public/rotas.html'));
-});
-
-router.get('/mudarSenha',(req,res)=>{
-  res.sendFile(path.join(__dirname+'/public/senha.html'));
-});
 module.exports= router;
--- server.js
+++ server.js
@@ -1,37 +1,15 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios').default;
 const app = express();
-const selects = require('./private/pgSelects.js');
 const routes = require('./routes');
 require('dotenv/config');
 
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/img'));
-// app.use(express.static('public'));
-// Rotas para renderizar as pagians Webs
 app.use(routes);
 
-// EndPoints
-app.get('/listarAlunos',selects.buscarAlunos);
-
-app.get('/listarTreinos',selects.buscarTreinos);
-
-app.get('/buscarTreinosPorAluno/:id',selects.buscarTreinosPorAlunoId);
-
-app.get('/buscarAluno/:id',selects.buscarAlunoId);
-
-app.get('/buscarTreino/:id',selects.buscarTreinoId);
-
-app.post('/criarAluno',selects.criarALuno);//precisa de um json
-
-app.get('/login/:usuario',selects.buscarAlunoPorLogin);
-
-app.post('/updateCliente/:id&:nome&:telefone&:email',selects.alterarDadosPessoais);
 
 var dia = new Date();
-console.log("Iniciando a API TecFit. "+String(dia.getDate()).padStart(2, '0')+"/"+String(dia.getMonth() + 1).padStart(2, '0')+"/"+dia.getFullYear()+" "+dia.getHours()+":"+dia.getMinutes()+":"+dia.getSeconds());
+console.log("Iniciando o Tibia Calc Damage. "+String(dia.getDate()).padStart(2, '0')+"/"+String(dia.getMonth() + 1).padStart(2, '0')+"/"+dia.getFullYear()+" "+dia.getHours()+":"+dia.getMinutes()+":"+dia.getSeconds());
 
 
 app.listen(process.env.PORT || 3333, ()=>{
